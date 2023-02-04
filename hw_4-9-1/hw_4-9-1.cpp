0ff62183be453f7716d15ae88632d4c20ebee555
Artyom Kazakov <kasakkainen@mail.ru>
2023-02-04 19:41:50 +0300

    first commit

---
+++ hw_4-9-1/hw_4-9-1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+class Fraction {
+
+private:
+	float numerator_;
+	float denominator_;
+
+public:
+	Fraction(float numerator, float denumerator) {
+		numerator_ = numerator;
+		denominator_ = denumerator;
+	}
+
+	bool operator == (Fraction other) {
+		return (this->numerator_ == other.numerator_) && (this->denominator_ == other.denominator_);
+	}
+	bool operator != (Fraction other) {
+		return (this->numerator_ != other.numerator_) && (this->denominator_ != other.denominator_);
+	}
+	bool operator < (Fraction other) {
+		return ((this->numerator_ / this->denominator_) < (other.numerator_ / other.denominator_));
+	}
+	bool operator > (Fraction other) {
+		return ((this->numerator_ / this->denominator_) > (other.numerator_ / other.denominator_));
+	}
+	bool operator <= (Fraction other) {
+		return ((this->numerator_ / this->denominator_) <= (other.numerator_ / other.denominator_));
+	}
+	bool operator >= (Fraction other) {
+		return ((this->numerator_ / this->denominator_) >= (other.numerator_ / other.denominator_));
+	}
+};
+
+int main() {
+	Fraction f1(4, 3);
+	Fraction f2(6, 11);
+
+	std::cout << "f1" << ((f1 == f2) ? " == " : " not == ") << "f2" << std::endl;
+	std::cout << "f1" << ((f1 != f2) ? " != " : " not != ") << "f2" << std::endl;
+	std::cout << "f1" << ((f1 < f2) ? " < " : " not < ") << "f2" << std::endl;
+	std::cout << "f1" << ((f1 > f2) ? " > " : " not > ") << "f2" << std::endl;
+	std::cout << "f1" << ((f1 <= f2) ? " <= " : " not <= ") << "f2" << std::endl;
+	std::cout << "f1" << ((f1 >= f2) ? " >= " : " not >= ") << "f2" << std::endl;
+}
